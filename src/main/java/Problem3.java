867b4a61da21d6581d0b78893c5b57caa9f1bd21
Khaled EL-Guindy <Kelguindy@msn.com>
2024-01-25 23:34:36 -0800

    Problem 2 work

--- src/main/java/Problem3.java
+++ src/main/java/Problem3.java
@@ -3,10 +3,42 @@ public class Problem3 {
     // Do not change signature (function name, parameters, return type)
     public static long SumOfNonUnique(int[] data) {
         // TODO: Add your solution
-        return -1; // Placeholder. TODO: Replace this line with your own code.
-    }
+        long sumOfNonUnique = 0;
+        int counter = 0;
+        int check= 0;
+        int[] repeated = new int[data.length];;
+        int[] nonUnique = new int[data.length];
+
+        for (int i = 0; i < data.length - 1; i++) // get the first element
+        {
+            for (int t = 0; t < nonUnique.length; t++) {
+                if (data[i] == nonUnique[t]) {
+                    check = 1;
+                }
+            }
+            if (check == 0) {
+                repeated[counter] = 0;
+                for (int j = i + 1; j < data.length; j++) // get the following elements to compare
+                {
+                    if (data[i] == data[j]) {
+                        nonUnique[counter] = data[i];
+                        repeated[counter] = repeated[counter] + 1;
+                    }
+
+                }
+                counter = counter + 1;
+            }
+        }
+
+        for (int t = 0; t < nonUnique.length; t++) {
+            sumOfNonUnique = sumOfNonUnique + nonUnique[t] * (repeated[t]+1);
+        }
+
+        return sumOfNonUnique; // Placeholder. TODO: Replace this line with your own code.
+
 
     /*
     TODO: Bonus Points. Add your answer here.
      */
+    }
 }
--- src/test/java/Problem3Test.java
+++ src/test/java/Problem3Test.java
@@ -4,8 +4,7 @@
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class Problem3Test {
 
@@ -13,10 +12,29 @@ public class Problem3Test {
     // DO NOT REMOVE OR CHANGE the signature
     @Test
     public void testSumOfNonUnique() {
-        assertTrue(false); // TODO: Place-holder. Replace with your tests.
-    }
+        //assertTrue(false); // TODO: Place-holder. Replace with your tests.
 
+        int[][] inputs = {
+               {3, 2, 1},
+               {3, 2, 1, 3, 22, 14},
+               {3, 3, 3, 3, 3, 3, 3, 3},
+               {1, 1, 4, 4, 5, 6, 4, 8, 4, 5},
+               {1}
+        };
+        // TODO: add more test case inputs
+        int[] expects = {0, 6, 24, 28, 0};
+
+        assertEquals(inputs.length , expects.length);
+
+        for (int i = 0; i < inputs.length; i++) {
+            assertEquals(Problem3.SumOfNonUnique(inputs[i]), expects[i]);
+        }
+
+
+
+    }
     @Test
+
     public void largeArrayTest() {
         final int aLargeNumber = 100000;
         int[] input = new int[aLargeNumber];
@@ -42,4 +60,5 @@ public class Problem3Test {
         // try a better design in your code so this test finishes faster
         // TODO: how many seconds did your code improve between the first version and last version of your code?
     }
+
 }
